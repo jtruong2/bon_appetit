86e977699927f7373338a1c946b9c4606ea08d80
jtruong2 <jtruong2@comcast.net>
2017-06-06 10:05:25 -0600

    add iteration 2

--- lib/pantry.rb
+++ lib/pantry.rb
@@ -1,3 +1,42 @@
+require 'pry'
+#* Centi-Units -- Equals 100 Universal Units
+#* Milli-Units -- Equals 1/1000 Universal Units
 class Pantry
+  attr_reader :stock
+  def initialize
+    @stock = {}
+  end
+
+  def stock_check(item)
+    if stock[item] == nil
+      stock[item] = 0
+    else
+      stock[item]
+    end
+  end
+
+  def restock(item, quantity)
+    stock[item] += quantity
+  end
+
+  def convert_units(recipe)
+    #1. If the recipe calls for more than
+    #100 Units of an ingredient, convert it to Centi-units
+    #2. If the recipe calls for less than 1 Units of an ingredient,
+    #convert it to Milli-units
+    converted_ingredients = {}
+    recipe.ingredients.map do |k,v|
+      if v > 100
+         total = v/100
+         converted_ingredients[k] = {quantity: total.to_i, units: "Centi-Units"}
+      elsif v < 1
+        total = v * 1000
+        converted_ingredients[k] = {quantity: total.to_i, units: "Milli-Units"}
+      else
+        converted_ingredients[k] = {quantity: v, units: "Universal Units"}
+      end
+    end
+    converted_ingredients
+  end
 
 end
--- test/pantry_test.rb
+++ test/pantry_test.rb
@@ -1,7 +1,51 @@
 require './lib/pantry'
+require './lib/recipe'
 require 'minitest/autorun'
 require 'minitest/pride'
 
 class PantryTest < Minitest::Test
+  def test_it_exists
+    pantry = Pantry.new
+
+    assert_instance_of Pantry, pantry
+  end
+
+  def test_check_stock
+    pantry = Pantry.new
+
+    assert_equal 0, pantry.stock_check("Cheese")
+  end
+
+  def test_checking_and_adding_stock
+    pantry = Pantry.new
+
+    assert_equal ({}), pantry.stock
+    assert_equal 0 , pantry.stock_check("Cheese")
+
+    pantry.restock("Cheese", 10)
+    assert_equal 10, pantry.stock_check("Cheese")
+
+    pantry.restock("Cheese", 20)
+    assert_equal 30, pantry.stock_check("Cheese")
+
+  end
+
+  def test_convert_units
+    r = Recipe.new("Spicy Cheese Pizza")
+    r.add_ingredient("Cayenne Pepper", 0.025)
+    r.add_ingredient("Cheese", 75)
+    r.add_ingredient("Flour", 500)
+    pantry = Pantry.new
+    expected = ({"Cayenne Pepper" => {quantity: 25, units: "Milli-Units"},
+    "Cheese"=>{quantity: 75, units: "Universal Units"},
+    "Flour"=> {quantity: 5, units: "Centi-Units"}})
+
+    assert_equal expected, pantry.convert_units(r)
+
+  end
+
+  def test_convert_units_to_Milli_units
+
+  end
 
 end
